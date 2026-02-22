3d1f2ac0cf259312aaa21d81a3bd932ee0c691a9
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2026-02-23 00:58:27 +0100

    fix: scene damage review, hide blurred layers when not visible

--- src/workspaces/workspace_selector.rs
+++ src/workspaces/workspace_selector.rs
@@ -109,7 +109,8 @@ impl WorkspaceSelectorView {
         layer.set_position((0.0, -250.0), None);
         layer.set_opacity(0.0, None);
         view.set_layer(layer.clone());
-
+        layer.set_hidden(true);
+        
         let drop_targets = Arc::new(RwLock::new(Vec::new()));
         let drop_hover_index = Arc::new(RwLock::new(None));
         let pressed_action = Arc::new(RwLock::new(None));
