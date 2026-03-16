3382fca6e805ae8c34b27be68297244495c2f667
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2026-03-16 20:27:56 +0100

    fmt

--- src/shell/xdg.rs
+++ src/shell/xdg.rs
@@ -4,8 +4,7 @@ use layers::prelude::{taffy, Interpolate, Layer, Transition};
 use smithay::{
     desktop::{
         find_popup_root_surface, get_popup_toplevel_coords, layer_map_for_output,
-        PopupKeyboardGrab, PopupKind, PopupPointerGrab, Window, WindowSurface,
-        WindowSurfaceType,
+        PopupKeyboardGrab, PopupKind, PopupPointerGrab, Window, WindowSurface, WindowSurfaceType,
     },
     input::{pointer::Focus, Seat},
     output::Output,
